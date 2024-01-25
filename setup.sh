26129385f32d9fbf93ba65531a034515b68114dc
Dpbm <dpbm136@gmail.com>
2024-01-25 17:50:47 -0300

    fixed binary url

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget https://github.com/Dpbm/exif-frontend/releases/download/exif-12.74/exiftool.tar.gz && \
+wget https://github.com/Dpbm/exif-backend/releases/download/exif-12.74/exiftool.tar.gz && \
   tar -xvf exiftool.tar.gz && \
   rm -rf *.tar.gz && \
   mv Image-ExifTool-12.74 exiftool
